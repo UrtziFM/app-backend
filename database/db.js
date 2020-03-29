b0e68461b4f40f9b5ac890fb09cf6a6ee1e0ce49
UrtziFM <urtzi.arana13@gmail.com>
2020-03-29 10:01:41 +0200

    enviroment secured

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
    // db: 'mongodb://localhost:27017/formdb'
-    db: 'mongodb://heroku_3dc89hxc:u1jljfrc6fm210onves2b16k2i@ds347917.mlab.com:47917/heroku_3dc89hxc'
-  };
+    db: process.env.DATABASE_URI
+}
